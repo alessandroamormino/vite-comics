85cf8407e7245fa825b840cf39784097c42d7593
alessandroamormino <amormino.alessandro97@gmail.com>
2023-03-25 11:07:30 +0100

    feat: create AppNav component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script>
+import AppNav from "./components/AppNav.vue";
 export default {
   data() {
     return {
 
     }
   },
+  components: {
+    AppNav,
+  },
 }
 </script>
-<template></template>
-<style></style>
+<template>
+  <AppNav></AppNav>
+</template>
+<style lang="scss"></style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,88 @@
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                'Characters',
+                'Comics',
+                'Movies',
+                'Tv',
+                'Games',
+                'Collectibles',
+                'Videos',
+                'Fans',
+                'News',
+                'Shop'
+            ],
+        }
+    },
+}
+</script>
+<template>
+    <nav class="container-centered">
+        <div class="nav-right">
+            <img src="/img/dc-logo.png" alt="Logo">
+        </div>
+        <div class="nav-left">
+            <ul>
+                <li v-for="link in links"><a href="#">{{ link }}</a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+<style lang="scss" scoped>
+@use '../scss/variables.scss' as *;
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    // flex
+
+    padding: 1.5em 0;
+
+    .nav-left {
+        ul {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 2em;
+        }
+
+        a {
+            position: relative;
+
+            color: #000;
+            text-decoration: none;
+            text-transform: uppercase;
+
+            padding: 4em 0;
+
+            &:visited,
+            &:active,
+            &:focus {
+                text-decoration: none;
+            }
+
+            &:hover {
+                color: $main-color;
+
+                &::after {
+                    content: '';
+
+                    position: absolute;
+                    bottom: 0;
+                    right: 0;
+                    left: 0;
+
+                    width: 100%;
+                    height: 4px;
+
+
+                    background-color: $main-color;
+                }
+            }
+        }
+    }
+}
+</style>
